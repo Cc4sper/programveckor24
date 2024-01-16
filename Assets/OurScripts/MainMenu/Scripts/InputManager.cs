7ec726939d30e471a8167c98e6218d7e0183394a
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2024-01-16 13:56:01 +0100

    Settings size changes

--- Assets/OurScripts/MainMenu/Scripts/InputManager.cs
+++ Assets/OurScripts/MainMenu/Scripts/InputManager.cs
@@ -58,10 +58,6 @@ public class InputManager : MonoBehaviour
     {
         // Spara den valda tangen för knappen i spelarens inställningar
         PlayerPrefs.SetString("CustomKey" + buttonIndex, key);
-        if (buttonIndex == 0)
-        {
-            print("sparar höger input som " + key);
-        }
         PlayerPrefs.Save();
     }
 
