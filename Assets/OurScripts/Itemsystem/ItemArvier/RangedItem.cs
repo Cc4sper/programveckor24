6763e134d06c7b4acea495fc7a9dcc54c35b192b
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2024-01-17 14:24:21 +0100

    Sound for weapons

--- Assets/OurScripts/Itemsystem/ItemArvier/RangedItem.cs
+++ Assets/OurScripts/Itemsystem/ItemArvier/RangedItem.cs
@@ -7,7 +7,13 @@ public class RangedItem : WeaponItem
     public float ProjectileSpeed;
     public override void UseItem()
     {
+        if (ID==10)
         base.UseItem(); //creates a melee strike and adds melee force 
         newAttack.GetComponent<Rigidbody2D>().velocity = playerPos.up * ProjectileSpeed;
+        AudioManager.Instance.PlaySFX("SandGun");
+        if(ID==11)
+        base.UseItem(); //creates a melee strike and adds melee force 
+        newAttack.GetComponent<Rigidbody2D>().velocity = playerPos.up * ProjectileSpeed;
+        AudioManager.Instance.PlaySFX("MerfGun");
     }
 }
