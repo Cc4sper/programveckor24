a5efe42bf7c15d78000d5da87a76e2de07255674
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2024-01-17 16:25:07 +0100

    audio sfx inpmlimented fully.

--- Assets/OurScripts/Itemsystem/ItemArvier/MeleeItem.cs
+++ Assets/OurScripts/Itemsystem/ItemArvier/MeleeItem.cs
@@ -7,17 +7,21 @@ public class MeleeItem : WeaponItem
     public float meleeForce;
     public override void UseItem()
     {
-        if (ID == 8) { 
+        if (ID == 8) {
             base.UseItem(); //creates a melee strike and adds melee force 
         newAttack.GetComponent<Rigidbody2D>().AddForce(playerPos.up * meleeForce, ForceMode2D.Force);
             AudioManager.Instance.PlaySFX("Club");
     }
         if (ID == 7)
-        {
+        
             base.UseItem(); //creates a melee strike and adds melee force 
             newAttack.GetComponent<Rigidbody2D>().AddForce(playerPos.up * meleeForce, ForceMode2D.Force);
             AudioManager.Instance.PlaySFX("Sword");
-        }
+        if (ID == 14)
+
+            base.UseItem(); //creates a melee strike and adds melee force 
+        newAttack.GetComponent<Rigidbody2D>().AddForce(playerPos.up * meleeForce, ForceMode2D.Force);
+        AudioManager.Instance.PlaySFX("GroundSlam");
 
 
     }
