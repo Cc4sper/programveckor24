009ee160c6719e6eb8af0f9401104d0ddc37a2af
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2024-01-24 14:50:28 +0100

    achivements

--- Assets/Ink/Demos/Basic Demo/Scripts/AchivementManager.cs
+++ Assets/Ink/Demos/Basic Demo/Scripts/AchivementManager.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AchivementManager : MonoBehaviour
 {
@@ -11,17 +14,41 @@ public class AchivementManager : MonoBehaviour
     public int integer;
     public float floating_point;
 
+    public bool AchivementUnlocked(String achivementName)
+    {
+        if (achivements == null)
+            return false;
+
+        Achivement[] achivementsArray = achivements.ToArray();
+        Achivement a = Array.Find(achivementsArray, ach => achivementName == ach.title);
+
+        return a != null && !a.achieved;
+    }
+
     public void Start()
     {
         InitalizeAchivements();
     }
 
-    private void InitalizeAchivements()
+    private void InitalizeAchivements() //here's the achivements possible to get.
     {
         if (achivements != null)
             return;
         achivements = new List<Achivement>();
-        achivements.Add(new Achivement("Step by step", "Set your integer to or over 100", (object o) => integer <= 100));
+        achivements.Add(new Achivement("Press W", "Press the 'W' key", (object o) => Input.GetKeyDown(KeyCode.W)));
+    }
+    private void Update()
+    {
+        CheckAchivementCompletion();
+    }
+    private void CheckAchivementCompletion()
+    {
+        if (achivements == null)
+            return;
+
+        foreach (var achivement in achivements)
+            if (!achivement.achieved)
+                achivement.UpdateCompletion();
     }
 }
 public class Achivement
@@ -40,12 +67,39 @@ public class Achivement
 
     public void UpdateCompletion()
     {
-        Debug.Log($"{title}: |{description}");
-        achieved = true;
+        if (RequirementsMet()) //Checks if reqiurements have been met
+        {
+            Debug.Log($"{title}: {description} - Achievement Unlocked!");
+            achieved = true;
+        }
+        else
+        {
+            Debug.Log($"{title}: {description} - Achievement Locked.");
+            achieved = false;
+        }
     }
 
     public bool RequirementsMet()
     {
         return requirement.Invoke(null);
+    }
+}
+public class AchievementPopup : MonoBehaviour
+{
+    public GameObject popupPanel;
+    public TextMeshProUGUI achievementText;
+    public Image achievementImage;
+
+    public void ShowPopup(string description, Sprite image)
+    {
+        achievementText.text = description;
+        achievementImage.sprite = image;
+        popupPanel.SetActive(true);
+        // You can add animation logic here if needed.
+    }
+
+    public void HidePopup()
+    {
+        popupPanel.SetActive(false);
     }
 }
