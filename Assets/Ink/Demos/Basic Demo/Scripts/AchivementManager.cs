8ecc314b9150a7032e86121c75c08a16d2a473ac
WHTALI <william.hammarstrom@elev.ga.lbs.se>
2024-01-25 15:27:57 +0100

    Achievements working

--- Assets/Ink/Demos/Basic Demo/Scripts/AchivementManager.cs
+++ Assets/Ink/Demos/Basic Demo/Scripts/AchivementManager.cs
@@ -1,105 +1,52 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using TMPro;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+[System.Serializable]
+public class AchivementsGet {
+    [Header("Name/desc")]
+    public string achName;
+    public string description;
+    public TextMeshProUGUI achText;
+    [Header("Image/gameObj")]
+    public Sprite sprite;
+    public GameObject achObject;
+    [Header("Achieved")]
+    public bool Achieved;
+}
 
 public class AchivementManager : MonoBehaviour
 {
-    public List<Achivement> achivements;
+    public AchivementsGet[] AchievementsElement;
 
-    public int integer;
-    public float floating_point;
-
-    public bool AchivementUnlocked(String achivementName)
+    void Start()
     {
-        if (achivements == null)
-            return false;
-
-        Achivement[] achivementsArray = achivements.ToArray();
-        Achivement a = Array.Find(achivementsArray, ach => achivementName == ach.title);
-
-        return a != null && !a.achieved;
-    }
-
-    public void Start()
-    {
-        InitalizeAchivements();
-    }
-
-    private void InitalizeAchivements() //here's the achivements possible to get.
-    {
-        if (achivements != null)
-            return;
-        achivements = new List<Achivement>();
-        achivements.Add(new Achivement("Press W", "Press the 'W' key", (object o) => Input.GetKeyDown(KeyCode.W)));
-    }
-    private void Update()
-    {
-        CheckAchivementCompletion();
-    }
-    private void CheckAchivementCompletion()
-    {
-        if (achivements == null)
-            return;
-
-        foreach (var achivement in achivements)
-            if (!achivement.achieved)
-                achivement.UpdateCompletion();
-    }
-}
-public class Achivement
-{
-    public Achivement(string title, string description, Predicate<object> requirement)
-    {
-        this.title = title;
-        this.description = description;
-        this.requirement = requirement;
-    }
-    public string title;
-    public string description;
-    public Predicate<object> requirement;
-
-    public bool achieved;
-
-    public void UpdateCompletion()
-    {
-        if (RequirementsMet()) //Checks if reqiurements have been met
+        // Access and print information about each achievement
+        foreach (AchivementsGet achievement in AchievementsElement)
         {
-            Debug.Log($"{title}: {description} - Achievement Unlocked!");
-            achieved = true;
+            Debug.Log("Achievement: " + achievement.achName + ", Description: " + achievement.description + ", Unlocked: " + achievement.Achieved);
         }
-        else
+    }
+    public void Update()
+    {
+        UpdateAchivementUI();
+    }
+    public void UpdateAchivementUI()
+    {
+        foreach (AchivementsGet achievement in AchievementsElement)
         {
-            Debug.Log($"{title}: {description} - Achievement Locked.");
-            achieved = false;
+            if (achievement.achText != null)
+            {
+                if (achievement.Achieved)
+                    achievement.achText.text = "Has been unlocked: " + achievement.achName + "\nDescription: " + achievement.description;
+                else
+                    achievement.achText.text = "Hasn't been unlocked: " + achievement.achName + "\nDescription: " + achievement.description;
+            }
         }
     }
 
-    public bool RequirementsMet()
-    {
-        return requirement.Invoke(null);
-    }
-}
-public class AchievementPopup : MonoBehaviour
-{
-    public GameObject popupPanel;
-    public TextMeshProUGUI achievementText;
-    public Image achievementImage;
-
-    public void ShowPopup(string description, Sprite image)
-    {
-        achievementText.text = description;
-        achievementImage.sprite = image;
-        popupPanel.SetActive(true);
-        // You can add animation logic here if needed.
-    }
-
-    public void HidePopup()
-    {
-        popupPanel.SetActive(false);
-    }
 }
