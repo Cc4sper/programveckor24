abcfa7389872603f1dddf9a7f39519510812f11a
Miniitroll <leon.darby@elev.ga.lbs.se>
2024-02-22 10:44:48 +0100

    kommentartillmusik

--- Assets/OurScripts/audio/music.cs
+++ Assets/OurScripts/audio/music.cs
@@ -27,7 +27,7 @@ public class music : MonoBehaviour
         }
     }
 
-    private void Start()
+    private void Start() // Gör så att ljudet startar på noll i volym.
     {
         source = GetComponent<AudioSource>();
         source.volume = 0f;
@@ -41,13 +41,10 @@ public class music : MonoBehaviour
             source.Play();
             StartCoroutine(FadeMusic(true, source, fadeSec, PlayerPrefs.GetFloat("MusicVolume",1)));
         }
-        if (Input.GetKey(KeyCode.M))
-        {
-            Repeat();
-        }
     }
 
     private IEnumerator FadeMusic(bool fadeIn,AudioSource source,float duration,float targetVolume)
+    // Ändrar musikens volym enligt duration till target volume.
     {
         if (!fadeIn)
         {
@@ -65,21 +62,21 @@ public class music : MonoBehaviour
             source.volume = Mathf.Lerp(startVol, targetVolume, time / duration);
             yield return null;
         }
-        if (newclip != null)
+        if (newclip != null) // så länge det nya klippet finns så byter det till nytt klipp.
         {
             source.clip = newclip;
         }
         yield break;
     }
 
-    public void Repeat()
+    public void Repeat() // startar om ljud klippet med en fade in.
     {
         source.volume = 0f;
         source.Play();
-        StartCoroutine(FadeMusic(true, source, 2f, PlayerPrefs.GetFloat("MusicVolume",1)));
+        StartCoroutine(FadeMusic(true, source, 2f, PlayerPrefs.GetFloat("MusicVolume",1))); 
     }
 
-    public void SwitchMusic(AudioClip newClip, float duration)
+    public void SwitchMusic(AudioClip newClip, float duration) // används av andra koder när man ska ändra musiken.
     {
         if (canSwitch == true)
         {
